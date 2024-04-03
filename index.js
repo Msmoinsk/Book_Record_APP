cccd45f5c01ff400289b866b08f344bb60547236
Msmoinsk <moinsk5365@gmail.com>
2024-04-03 14:54:21 +0530

    Data testing done

--- index.js
+++ index.js
@@ -2,11 +2,42 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+const { users } = require("./data/users.json")
+
 const PORT = 8051
 
+/**
+ * Route : /
+ * Method : GET 
+*/ 
 app.get("/", (req,res) => {
     res.status(200).json({
         message:"Server is running",
+        data:"<h1>Hiiiii</h1>",
+    })
+})
+
+/**
+ * Route : /users
+ * Method : GET
+ * Description : Get all the users
+ * Access : Public
+ * Parameter : None {if in url we sent the ID hten it will be the [para : ID]}
+ */
+app.get("/users", (req,res) => {
+    res.status(200).json({
+        success : true,
+        data : users
+    })
+})
+
+
+
+
+
+app.get("*",(req,res) => {
+    res.status(404).json({
+        message:"This oute does not exist"
     })
 })
 
